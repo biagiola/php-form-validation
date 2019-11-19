d0c1e3ba1a1f4b67c8c0371945bdb842063974f4
David <41311088+biagiola@users.noreply.github.com>
2019-11-19 17:44:08 -0300

    Add files via upload

--- user_validator.php
+++ user_validator.php
@@ -7,11 +7,20 @@
         private static $fields = ['username', 'email'];
 
         public function __constructor( $post_data ) {
-            $this->data = $post_data; // $post_data is $_POST data that is grab from the form's values when we press the submit button
+            $this->data = $post_data; // $post_data is $_POST data that is grab from the form's values when we press the submit button.
         }
 
         public function validateForm() {
-             
+            foreach( self::$fields as $field ) {
+                // we ask if there is a field data [username and email] in the $data array that has the values from the inputs passed from the $_POST global variable.
+                if( !array_key_exists( $field, $this->data )) { // if username or email doesnt exist it will throw a error message.
+                    trigger_error("$field is not present in data" );
+                    return;
+                } 
+                // but if it exist then we validate
+                $this->validateUsername();
+                $this->validateEmail();
+            }
         }
 
         private function validateUsername() {
